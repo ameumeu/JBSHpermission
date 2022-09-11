cc4bac3f84c5c5af450d2e92a865682f7bb462a4
unknown <ameumeu113@gmail.com>
2022-09-11 13:54:06 +0900

    idk

--- permission/views.py
+++ permission/views.py
@@ -3,37 +3,21 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import check_password
 from django.utils import timezone
 
-from .models import User
 
 
 # Create your views here.
-def index(request):    
-    if request.COOKIES.get('username') is not None:
-        username = request.COOKES.get('username')
-        password = request.COOKIES.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect("permission:index")
-        else:
-            return redirect("permission:index")
-
-    elif request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            response.set_cookie('username', username)
-            response.set_cookie('password', password)
-            response = redirect("permission:index")
-            return response
-        else:
-            return render(request, 'account/login.html', {'error':'다시 입력해주세요.'})
-    
-    else:
-        return render(request, "permission/index.html")
+def index(request): 
+    try: #request.session['username'] is not None:
+        print('nohai')
+        username = request.session['username']
+        password = request.session['password']
+        error = request.session['error']
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        print('ready')
+        return render(request, "permission/logout.html")
+    except:
+        return render(request, 'permission/index.html')
     
 
 def signup(request):
@@ -49,28 +33,4 @@ def signup(request):
         return redirect(reverse("permission:index"))
     return render(request, 'permission/signup.html')
 
-def logout(request):
-    response = render(request, 'permission/index.html')
-    response.delete_cookie('username')
-    response.delete_cookie('password')
-    logout(request)
-    return response
 
-def change_pw(request):
-    context = {}
-    if request.method == "POST":
-        old_password = request.POST.get("old_password")
-        user = request.user
-        if check_password(old_password, user.password):
-            password1 = request.POST.get("password1")
-            password2 = request.POST.get("password2")
-            if password1 == password2:
-                user.set_password(password1)
-                user.save()
-                login(request, user)
-                return redirect('permission:index')
-            else:
-                context.update({'error':'새 비밀번호가 확인되지 않았습니다.'})
-        else:
-            context.update({'error':'현재 비밀번호가 틀립니다.'})
-    return render(request, 'permission/change_pw.html', context)
